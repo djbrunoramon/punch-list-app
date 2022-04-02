b07633502d55c4189f39ca5bafd72f5cf2496775
Bruno Ramon <brmachado_5@hotmail.com>
2022-04-02 15:46:03 -0300

    Added url for aws

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,8 @@
 const PROXY_CONFIG = [
   {
     context: ['/api/v1'],
-    target: 'http://localhost:8080/',
+    target: 'http://punchlistapi-env.eba-5symj6wc.us-east-1.elasticbeanstalk.com/',
+    // target: 'http://localhost:8080/',
     secure: false,
     logLevel: 'debug'
   }
