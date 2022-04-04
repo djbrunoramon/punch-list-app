9c347ce0a7c881dbeb72506e0a7df5246a7e06e9
Bruno Ramon <brmachado_5@hotmail.com>
2022-04-03 23:18:59 -0300

    Adjusted configs

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,8 +1,7 @@
 const PROXY_CONFIG = [
   {
     context: ['/api/v1'],
-    target: 'http://punchlistapi-env.eba-5symj6wc.us-east-1.elasticbeanstalk.com/',
-    // target: 'http://localhost:8080/',
+    target: 'http://localhost:8080/',
     secure: false,
     logLevel: 'debug'
   }
